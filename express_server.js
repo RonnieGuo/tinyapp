6ab81601fc740520470c00505bd106af10a97b2b
RonnieGuo <guorongfly@gmail.com>
2023-06-25 15:54:45 -0600

    Sending Data to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -21,9 +21,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/set", (req, res) => {
   const a = 1;
@@ -33,3 +33,16 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// the templateVars object contains the string 'Hello World' under the key greeting. We then pass the templateVars object to the template called hello_world.
+
+//In our hello_world.ejs file, we can display the 'Hello World!' string stored in the templateVars object by calling the key greeting:
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
